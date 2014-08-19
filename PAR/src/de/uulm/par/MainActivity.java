aed6108e00fceac8e3794177dbda140aa225685d
dafuzzi <Fuzzi@eduroam161-038.wlan.uni-ulm.de>
2014-08-19 16:39:38 +0200

    updated guy

--- PAR/src/de/uulm/par/MainActivity.java
+++ PAR/src/de/uulm/par/MainActivity.java
@@ -1,39 +1,65 @@
 package de.uulm.par;
 
+import java.util.HashMap;
+import java.util.List;
 import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
+	ListView list;
+	String[] web = { "Time based", "Location based", "Simple note", "Person based", "Location based",
+			"Simple note", "Person based" };
+	Integer[] imageId = { R.drawable.ic_time, R.drawable.ic_location,
+			R.drawable.ic_note, R.drawable.ic_person,
+			R.drawable.ic_location, R.drawable.ic_note,
+			R.drawable.ic_person};
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		CustomList adapter = new CustomList(MainActivity.this, web, imageId);
+		list = (ListView) findViewById(R.id.list);
+		list.setAdapter(adapter);
+		list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position,
+					long id) {
+				Toast.makeText(MainActivity.this,
+						"You Clicked at " + web[+position], Toast.LENGTH_SHORT)
+						.show();
+				
+			}
+		});
+	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_add_reminder) {
-        	Intent intent = new Intent(this, ListViewExampleActivity.class);
-            startActivity(intent);
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_add_reminder) {
+			Intent intent = new Intent(this, AddNote.class);
+			startActivity(intent);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
