7f9d3858bcc55232fd9b83d944edd8b22f3957af
dafuzzi <Fuzzi@eduroam161-038.wlan.uni-ulm.de>
2014-08-27 12:44:36 +0200

    all views

--- PAR/src/de/uulm/par/notes/AddLocation.java
+++ PAR/src/de/uulm/par/notes/AddLocation.java
@@ -1,8 +1,15 @@
 package de.uulm.par.notes;
 
+import org.joda.time.DateTime;
+
 import de.uulm.par.R;
 
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.content.Intent;
 import android.os.Bundle;
 
 
@@ -12,6 +19,25 @@ public class AddLocation extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add_location);
+		
+		final TextView title = (TextView) findViewById(R.id.new_location_title);
+		final TextView message = (TextView) findViewById(R.id.new_location_message);
+
+		Button add = (Button) findViewById(R.id.btn_add);
+		add.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				PlainNote addingNote = new PlainNote(
+						title.getText().toString(), message.getText()
+								.toString(), NoteType.LOCATION, new DateTime());
+				addingNote.setLocation("Uni Ulm");
+				Intent returnIntent = new Intent();
+				returnIntent.putExtra("Note",addingNote);
+				setResult(RESULT_OK,returnIntent);
+				finish();
+			}
+		});
 	}
 
 }
--- PAR/src/de/uulm/par/notes/AddPerson.java
+++ PAR/src/de/uulm/par/notes/AddPerson.java
@@ -1,16 +1,42 @@
 package de.uulm.par.notes;
 
+import org.joda.time.DateTime;
+
 import de.uulm.par.R;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.content.Intent;
 import android.os.Bundle;
 
-
 public class AddPerson extends ActionBarActivity {
+	private String personMac;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add_person);
+
+		final TextView title = (TextView) findViewById(R.id.new_location_title);
+		final TextView message = (TextView) findViewById(R.id.new_location_message);
+
+		Button add = (Button) findViewById(R.id.btn_add);
+		add.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				PlainNote addingNote = new PlainNote(
+						title.getText().toString(), message.getText()
+								.toString(), NoteType.PERSON, new DateTime());
+				addingNote.setLocation("Uni Ulm");
+				Intent returnIntent = new Intent();
+				returnIntent.putExtra("Note", addingNote);
+				setResult(RESULT_OK, returnIntent);
+				finish();
+			}
+		});
 	}
 
 }
